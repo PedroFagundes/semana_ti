981b2e34d81b50ae8af99e42b47cf18e9e43c360
Pedro Henrique Fagundes <pedrofagundesb@gmail.com>
2016-08-19 18:00:04 -0300

    Add subscription feedback message

--- semana_ti/homesite/views.py
+++ semana_ti/homesite/views.py
@@ -1,11 +1,14 @@
+# -*- coding: utf-8 -*-
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import CreateView
 
 from subscription.models import Subscription
 from subscription.forms import SubscriptionForm
 
 
-class Home(CreateView):
+class Home(SuccessMessageMixin, CreateView):
 	template_name = 'homesite/home.html'
 	model = Subscription
 	form_class = SubscriptionForm
 	success_url = '/'
+	success_message = 'Sucesso!'

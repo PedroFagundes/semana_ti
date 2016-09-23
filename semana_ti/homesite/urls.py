d4cc8304efb1042689f5aaf122a1c93dc66b4c61
Pedro Henrique Fagundes <pedrofagundesb@gmail.com>
2016-09-23 12:01:57 -0300

    Work on mail messagin

--- semana_ti/homesite/urls.py
+++ semana_ti/homesite/urls.py
@@ -4,4 +4,5 @@ from homesite import views
 
 urlpatterns = [
     url(r'^$', views.Home.as_view(), name='home'),
+    url(r'^send-mails$', views.send_confirmation_mail_to_all, name='send_mails'),
 ]
--- semana_ti/homesite/views.py
+++ semana_ti/homesite/views.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.contrib.messages.views import SuccessMessageMixin
+from django.core.urlresolvers import reverse_lazy
+from django.http import HttpResponseRedirect
+from django.template.loader import render_to_string
 from django.views.generic import CreateView
 
 from subscription.models import Subscription
 from subscription.forms import SubscriptionForm
+from subscription.utils import send_html_mail
 
 
 class Home(SuccessMessageMixin, CreateView):
@@ -12,3 +16,13 @@ class Home(SuccessMessageMixin, CreateView):
 	form_class = SubscriptionForm
 	success_url = '/'
 	success_message = 'Sucesso!'
+
+
+def send_confirmation_mail_to_all(request):
+	subscriptions = Subscription.objects.all()[:5]
+	import pdb; pdb.set_trace()
+	for subscription in subscriptions:
+		html_message = render_to_string('mail/e-mail.html', {'subscription': subscription})
+		send_html_mail('Confirmacao de inscricao', html_message, ['pedrofagundesb@gmail.com'])
+	return HttpResponseRedirect(reverse_lazy('home'))
+
